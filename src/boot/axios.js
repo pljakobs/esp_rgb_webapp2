d1bd1145e1bb20ef5ac71e4c9a38ccee5bf7b3a7
Peter Jakobs <pjakobs@mobileme.de>
2023-11-21 15:26:35 +0100

    axios config

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,5 @@
-import axios from 'axios' const axiosInstance = axios.create({
+import axios from 'axios'
+const axiosInstance = axios.create({
   baseURL: 'http://192.168.29.38'
 })export default ({ Vue }) => {  Vue.prototype.$axios = axiosInstance
 }export { axiosInstance }
