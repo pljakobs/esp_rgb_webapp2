49184f14cdb04bec087ae647596570b7af4ce555
Peter Jakobs <pjakobs@mobileme.de>
2023-11-24 11:45:53 +0100

    trying to use vuex-map-fields to simplify access to the json properties

--- src/store/modules/config.js
+++ src/store/modules/config.js
@@ -1,26 +1,37 @@
 // store/modules/config.js
-import axios from 'axios';
-
-const API_BASE_URL = 'http://192.168.29.38';
+import axios from "axios";
+import { mapFields } from "vuex-map-fields";
+const API_BASE_URL = "http://192.168.29.38";
 
 function generatePartialData(currentState, path) {
-    const keys = path.split('.');
-    let result = {};
-    let temp = currentState;
+  const keys = path.split(".");
+  let result = {};
+  let temp = currentState;
 
-    for (let i = 0; i < keys.length ; i++) {
-      const key = keys[i];
-      temp = temp[key];
-    }
-    for (let i=keys.length -1 ;i>=0; i--) {
-        const key = keys[i];
-        result={};
-        result[key]=temp;
-        temp=result;
-    }
-    return result;
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    temp = temp[key];
   }
-
+  for (let i = keys.length - 1; i >= 0; i--) {
+    const key = keys[i];
+    result = {};
+    result[key] = temp;
+    temp = result;
+  }
+  return result;
+}
+function generateFieldMappings(state, path = "") {
+  const mappings = {};
+  for (const key in state) {
+    const fullPath = path ? `${path}.${key}` : key;
+    mappings[key] = fullPath;
+    if (typeof state[key] === "object") {
+      Object.assign(mappings, generateFieldMappings(state[key], fullPath));
+    }
+  }
+  return mappings;
+}
+export { generateFieldMappings };
 const state = {
   configData: null,
 };
@@ -35,44 +46,50 @@ const actions = {
   async fetchConfigData({ commit }) {
     try {
       const response = await axios.get(`${API_BASE_URL}/config`);
-      commit('setConfigData', response.data);
+      commit("setConfigData", response.data);
     } catch (error) {
-      console.error('Error fetching config data:', error);
+      console.error("Error fetching config data:", error);
     }
   },
 
   async updateConfigData({ commit, state }, path) {
-
     try {
       const partialData = generatePartialData(state.configData, path);
-      console.log(`trying to update the API with partial data`,JSON.stringify(partialData));
-      await axios.post(`${API_BASE_URL}/config`, JSON.stringify(partialData),{
+      console.log(
+        `trying to update the API with partial data`,
+        JSON.stringify(partialData)
+      );
+      await axios.post(`${API_BASE_URL}/config`, JSON.stringify(partialData), {
         headers: {
-            // 'application/json' is the modern content-type for JSON, but some
-            // older servers may use 'text/json'.
-            // See: http://bit.ly/text-json
-            'content-type': 'text/json'
-      }
-    });
+          // 'application/json' is the modern content-type for JSON, but some
+          // older servers may use 'text/json'.
+          // See: http://bit.ly/text-json
+          "content-type": "text/json",
+        },
+      });
       //await this.dispatch('fetchConfigData');
     } catch (error) {
-      console.error('Error updating config data:', error);
+      console.error("Error updating config data:", error);
     }
   },
 };
 
-const watch = {
-    configData(newConfigData, oldConfigData) {
-      // Perform actions when configData changes
-      console.log('configData changed:', newConfigData);
-      // Derive the path of the changed property and take necessary actions
-    },
-  };
+const getters = {
+  fieldMappings: (state) => generateFieldMappings(state.configData),
+};
 
+const watch = {
+  configData(newConfigData, oldConfigData) {
+    // Perform actions when configData changes
+    console.log("configData changed:", newConfigData);
+    // Derive the path of the changed property and take necessary actions
+  },
+};
 
 export default {
   namespaced: true,
   state,
   mutations,
   actions,
+  getters,
 };
