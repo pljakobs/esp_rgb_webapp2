f728a1fb5894dce4110e7e05c8675da43e0460ce
Peter Jakobs <pjakobs@mobileme.de>
2024-01-20 14:17:28 +0100

    selecting and connecting to a network now works

--- src/pages/NetworkInit.vue
+++ src/pages/NetworkInit.vue
@@ -1,101 +1,165 @@
 <template>
-  <q-page padding>
-    <div id="q-app" class="bg-blue-grey-2" style="min-height: 100vh">
-      <div
-        id="parent"
-        class="fit row wrap justify-center items-start content-start"
-      >
-        <div
-          class="col-xs-12 col-sm-5 col-md-5 col-lg-4 q-gutter-md"
-          justify-center
+  <div>
+    <q-card bordered class="my-card shadow-4 col-auto fit q-gutter-md">
+      <div class="row justify-center">
+        <h4>application initialization</h4>
+        <q-select
+          filled
+          v-model="selectedNetwork"
+          :options="networks"
+          :label="selectedNetwork ? 'Network selected' : 'Select a network'"
+          hint="Select a network from the list"
+          option-label="ssid"
+          option-value="ssid"
+          style="width: 80%"
         >
-          application initialization
-          <q-select
-            filled
-            v-model="selectedNetwork"
-            :options="networks"
-            label="Select a network"
-            hint="Select a network from the list"
-            option-label="ssid"
-            option-value="ssid"
-          />
-          {{ JSON.stringify(networks, null, 2) }}
-        </div>
+          <template v-slot:option="props">
+            <q-item
+              v-bind="props.itemProps"
+              style="display: flex; justify-content: space-between; width: 100%"
+            >
+              <q-item-section>
+                <q-icon :name="getSignalIcon(props.opt.signal)" />
+              </q-item-section>
+              <q-item-section>
+                {{ props.opt.ssid }}
+              </q-item-section>
+              <q-item-section>
+                <q-icon :name="getEncryptionIcon(props.opt.encryption)" />
+              </q-item-section>
+            </q-item>
+          </template>
+        </q-select>
+
+        <q-input
+          filled
+          v-model="selectedNetwork"
+          :label="selectedNetwork ? 'SSID' : 'Enter SSID'"
+          hint="Enter the SSID of the network"
+          style="width: 80%"
+        />
+
+        <q-input
+          filled
+          v-model="password"
+          label="Password"
+          type="password"
+          hint="Enter the password for the network"
+          style="width: 80%"
+        />
+
+        <q-btn
+          color="primary"
+          label="Connect"
+          @click="connectToNetwork"
+          style="margin-top: 16px"
+        />
       </div>
-    </div>
-  </q-page>
+    </q-card>
+  </div>
 </template>
 
 <script>
-import { ref, onMounted } from "vue";
-
-const maxRetries = 5; // Maximum number of retries
-const retryDelay = 1000; // Delay for the first retry in milliseconds
+import { ref, onMounted, watch } from "vue";
 
 export default {
   setup() {
-    const networks = ref(null);
-
+    const selectedNetwork = ref(null);
+    const networks = ref([]);
+    const password = ref("");
+    const ssid = ref("");
     const ip_address = window.location.hostname;
-    console.log("ip address:", ip_address);
+
+    const connectToNetwork = async () => {
+      console.log("Connecting to network:", ssid.value);
+      console.log("Password:", password.value);
+
+      const response = await fetch(`http://${ip_address}/connect`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ssid: ssid.value,
+          password: password.value,
+        }),
+      });
+
+      if (response.ok) {
+        console.log("Connected to network");
+      } else {
+        console.log("Failed to connect to network");
+      }
+    };
+
+    watch(selectedNetwork, (newVal) => {
+      ssid.value = newVal;
+    });
 
     const fetchNetworks = async (retryCount = 0) => {
-      if (ip_address !== null) {
-        console.log("fetching networks");
-        // step 1: get the list of available networks
-        let response = await fetch(`http://${ip_address}/networks`, {
-          method: "GET",
-        });
-        if (response.status === 429 && retryCount < maxRetries) {
-          // Too many requests, retry after a delay
-          console.log(
-            `Request limit reached, retrying after ${
-              retryDelay * 2 ** retryCount
-            }ms...`,
-          );
-          setTimeout(
-            () => fetchNetworks(retryCount + 1),
-            retryDelay * 2 ** retryCount,
-          );
-          return;
-        }
-        networks.value = await response.json();
-        console.log("networks", JSON.stringify(networks.value));
+      try {
+        if (ip_address !== null) {
+          console.log("fetching networks");
+          // step 1: get the list of available networks
+          let response = await fetch(`http://${ip_address}/networks`, {
+            method: "GET",
+          });
+          if (response.status === 429 && retryCount < maxRetries) {
+            // Too many requests, retry after a delay
+            console.log(
+              `Request limit reached, retrying after ${
+                retryDelay * 2 ** retryCount
+              }ms...`,
+            );
+            setTimeout(
+              () => fetchNetworks(retryCount + 1),
+              retryDelay * 2 ** retryCount,
+            );
+            return;
+          }
+          let responseJson = await response.json();
+          responseJson.available.sort((a, b) => {
+            return b.signal - a.signal;
+          });
+          networks.value = responseJson["available"];
+          console.log("networks", JSON.stringify(networks.value));
 
-        // step 2: scan for available wifi networks
-        response = await fetch(`http://${ip_address}/scan_networks`, {
-          method: "POST",
-        });
-        if (response.status === 429 && retryCount < maxRetries) {
-          // Too many requests, retry after a delay
-          console.log(
-            `Request limit reached, retrying after ${
-              retryDelay * 2 ** retryCount
-            }ms...`,
-          );
-          setTimeout(
-            () => fetchNetworks(retryCount + 1),
-            retryDelay * 2 ** retryCount,
-          );
-          return;
+          // step 2: scan for available wifi networks
+          response = await fetch(`http://${ip_address}/scan_networks`, {
+            method: "POST",
+          });
+          if (response.status === 429 && retryCount < maxRetries) {
+            // Too many requests, retry after a delay
+            console.log(
+              `Request limit reached, retrying after ${
+                retryDelay * 2 ** retryCount
+              }ms...`,
+            );
+            setTimeout(
+              () => fetchNetworks(retryCount + 1),
+              retryDelay * 2 ** retryCount,
+            );
+            return;
+          }
         }
-        let responseJson = await response.json();
-        responseJson.available.sort((a, b) => {
-          return b.signal - a.signal;
-        });
-        networks.value = responseJson["available"];
-        console.log("networks", JSON.stringify(networks.value));
-
-        console.log("==> response.available:", responseJson.available);
+      } catch (error) {
+        console.log("error fetching networks", error);
+      } finally {
+        setTimeout(() => fetchNetworks(), 15000); // fetch networks ever 15s
       }
     };
 
     onMounted(() => {
       fetchNetworks();
-      setInterval(fetchNetworks, 10000); // Re-run every 10 seconds
     });
 
-    return { networks };
+    return {
+      selectedNetwork,
+      networks,
+      password,
+      ssid,
+      connectToNetwork,
+    };
   },
 };
 </script>
