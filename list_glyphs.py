5ea3dc3e02893442694af7fbd84cfb414af3c207
Peter Jakobs <pjakobs@mobileme.de>
2024-04-20 13:34:39 +0200

    started to move to svg icons

--- list_glyphs.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-from fontTools.ttLib import TTFont
-#font = TTFont("./node_modules/@quasar/extras/material-icons/web-font/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.woff")
-#font = TTFont("./dist/spa/assets/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.woff")
-font = TTFont("./dist/spa/assets/font1.woff")
-glyph_set = font.getGlyphSet()
-for glyph_name in glyph_set.keys():
-    print(glyph_name)
