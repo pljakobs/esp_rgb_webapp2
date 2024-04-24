16c0f7e437b398c56537c342d11db9077d79cf75
Peter Jakobs <pjakobs@mobileme.de>
2024-04-24 18:55:23 +0200

    added three more icons in networkSettings

--- src/pages/testPage.vue
+++ src/pages/testPage.vue
@@ -3,33 +3,22 @@
     <q-card-section>
       <h2>Test Page</h2>
     </q-card-section>
-    <q-card-section>
-      <h2><q-icon :name="outlinedHome" /></h2>
-    </q-card-section>
+    <q-card-section> free heap: {{ infoData.data.heap_free }} </q-card-section>
   </q-card>
 </template>
 
 <script>
 import { ref, watch } from "vue";
 import { outlinedHome } from "@quasar/extras/material-icons-outlined";
+import { infoDataStore } from "src/stores/infoDataStore";
 
 export default {
   setup() {
-    const showDialog = ref(false);
-    const doShowDialog = () => {
-      showDialog.value = true;
-      console.log("showDialog (showing)", showDialog.value);
-    };
+    const infoData = infoDataStore();
 
-    const doHideDialog = () => {
-      showDialog.value = false;
-      console.log("showDialog (hiding)", showDialog.value);
+    return {
+      infoData,
     };
-
-    watch(showDialog, (newVal) => {
-      console.log("showDialog changed", newVal);
-    });
-    return { outlinedHome, doShowDialog, doHideDialog, showDialog };
   },
 };
 </script>
