2d7ad23cf3e21f73fec5242b562141b0657d08d0
Peter Jakobs <pjakobs@mobileme.de>
2024-10-25 00:47:10 +0200

    fixed heap display on testing page

--- src/pages/testPage.vue
+++ src/pages/testPage.vue
@@ -3,19 +3,34 @@
     <q-card-section>
       <h2>Test Page</h2>
     </q-card-section>
-    <q-card-section> free heap: {{ infoData.data.heap_free }} </q-card-section>
+    <q-card-section>
+      free heap: {{ infoData.data.heap_free }}<br />
+      <button @click="refreshData">Refresh Data</button>
+    </q-card-section>
   </q-card>
 </template>
 
 <script>
+import { onMounted } from "vue";
 import { infoDataStore } from "src/stores/infoDataStore";
 
 export default {
   setup() {
     const infoData = infoDataStore();
 
+    // Function to refresh data
+    const refreshData = () => {
+      infoData.fetchData();
+    };
+
+    // Fetch data when the component is mounted
+    onMounted(() => {
+      refreshData();
+    });
+
     return {
       infoData,
+      refreshData,
     };
   },
 };
