e8033ffff1a9fed3bb65d498d86e2b85bd86ecc3
Peter Jakobs <pjakobs@mobileme.de>
2025-02-12 13:24:17 +0100

    functional log viewer

--- src/components/LogViewerCard.vue
+++ src/components/LogViewerCard.vue
@@ -1,12 +1,14 @@
 <template>
-  <MyCard icon="article" title="view log">
+  <MyCard icon="article" title="Log Viewer">
     <q-card-section class="row justify-center no-padding">
       <div class="log-viewer">
-        <h2>Log Viewer</h2>
         <div v-for="(log, index) in logs" :key="index" class="log-line">
           {{ log }}
         </div>
       </div>
+    </q-card-section>
+    <q-card-section class="row justify-center no-padding">
+      <q-btn @click="downloadLogFile" label="Download Log" color="primary" />
     </q-card-section>
   </MyCard>
 </template>
@@ -16,27 +18,44 @@ import { logStore } from "src/services/logServices";
 import MyCard from "src/components/myCard.vue";
 
 export default {
-  name: "LogViewer",
+  name: "LogViewerCard",
   components: {
     MyCard,
   },
   setup() {
+    const downloadLogFile = () => {
+      const logContent = logStore.logs.join("\n");
+      const blob = new Blob([logContent], { type: "text/plain" });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "Lightinator_log.txt";
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      URL.revokeObjectURL(url);
+    };
+
     return {
       logs: logStore.logs,
+      downloadLogFile,
     };
   },
 };
 </script>
-
 <style scoped>
 .log-viewer {
   padding: 20px;
-  background-color: #f0f0f0;
+  background-color: #505050;
   border-radius: 5px;
+  max-height: 400px; /* Set a maximum height for the log viewer */
+  max-width: 100%; /* Set a maximum width for the log viewer */
+  overflow: auto; /* Enable scrolling in both directions */
 }
 
 .log-line {
   margin-bottom: 10px;
   font-family: monospace;
+  white-space: pre-wrap; /* Preserve whitespace and wrap long lines */
 }
 </style>
