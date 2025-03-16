5ccd0e7ece5d8c28d5a4fe58c28e376a98da80a9
Peter Jakobs <pjakobs@mobileme.de>
2025-03-16 12:40:34 +0100

    minor change to trigger rebuild

--- src/components/myCard.vue
+++ src/components/myCard.vue
@@ -64,7 +64,7 @@ export default {
 
 <style scoped>
 .rotate-icon {
-  transition: transform 0.3s ease;
+  transition: transform 0.4s ease;
   cursor: pointer;
   transform-origin: center center;
 }
