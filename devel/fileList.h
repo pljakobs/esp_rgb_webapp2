537bf92a3c302368c2b4780af6b87c9813e4c163
pljakobs <pljakobs@users.noreply.github.com>
2025-11-06 14:04:21 +0000

    deploy: 2ea66341b6d8de6d9808089b563f11ac334dc4d3

---
+++ devel/fileList.h
@@ -0,0 +1,12 @@
+#define FILE_LIST(XX) \
+	XX(VERSION, "VERSION") \
+	XX(RgbwwLayout_css_gz, "assets/RgbwwLayout.css.gz") \
+	XX(RgbwwLayout_js_gz, "assets/RgbwwLayout.js.gz") \
+	XX(i18n_js_gz, "assets/i18n.js.gz") \
+	XX(index_css_gz, "assets/index.css.gz") \
+	XX(index_js_gz, "assets/index.js.gz") \
+	XX(registerSvgIcon_js_gz, "assets/registerSvgIcon.js.gz") \
+	XX(pinconfig_json, "config/pinconfig.json") \
+	XX(favicon_ico, "icons/favicon.ico") \
+	XX(iconsSprite_svg_gz, "icons/iconsSprite.svg.gz") \
+	XX(index_html_gz, "index.html.gz")
