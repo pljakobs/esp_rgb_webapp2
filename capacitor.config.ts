f399dfba641898d66617f537af25192f29f1a093
Peter Jakobs <pjakobs@mobileme.de>
2025-12-18 23:31:44 +0100

    configuring capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lightinator.app',
+  appName: 'Lightinator',
+  webDir: 'dist'
+};
+
+export default config;
