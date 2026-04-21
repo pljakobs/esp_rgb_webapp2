eba13a130d04949897fa69e5756de8d2f4e52b19
pljakobs <pljakobs@users.noreply.github.com>
2026-04-21 10:29:21 +0000

    deploy: ef6e322698c709977debab940b41d926f4cc6865

--- devel/fileList.h
+++ devel/fileList.h
@@ -3,7 +3,6 @@
 	XX(RgbwwLayout_css_gz, "assets/RgbwwLayout.css.gz") \
 	XX(RgbwwLayout_js_gz, "assets/RgbwwLayout.js.gz") \
 	XX(autosaveFeedback_js_gz, "assets/autosaveFeedback.js.gz") \
-	XX(i18n_js_gz, "assets/i18n.js.gz") \
 	XX(index_css_gz, "assets/index.css.gz") \
 	XX(index_js_gz, "assets/index.js.gz") \
 	XX(registerSvgIcon_js_gz, "assets/registerSvgIcon.js.gz") \
